d14e128d60c52fb3da5b4d522d3ce637ef6e169d
coffee <985942825@qq.com>
2024-09-28 13:17:00 +0800

    将PlayerEntity的位置信息映射到GameObject世界

---
+++ Assets/Scripts/ObejctWorld/PlayerPositionMap.cs
@@ -0,0 +1,32 @@
+﻿using Cysharp.Threading.Tasks;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace VampireDynasty
+{
+    public class PlayerPositionMap : MonoBehaviour
+    {
+        private EntityManager _entityManager;
+        private EntityQuery playerEntityQuery;
+        private Entity playerEntity;
+        
+        private async void Start()
+        {
+            _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            playerEntityQuery = _entityManager.CreateEntityQuery(typeof(PlayerTag));
+            while (playerEntity == Entity.Null)
+            {
+                await UniTask.NextFrame();
+                if (playerEntityQuery.IsEmpty) continue;
+                playerEntity = playerEntityQuery.GetSingletonEntity();
+            }
+        }
+
+        private void Update()
+        {
+            if (playerEntity == Entity.Null) return;
+            transform.position = _entityManager.GetComponentData<LocalTransform>(playerEntity).Position;
+        }
+    }
+}
